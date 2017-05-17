c6bebe7236567cd54882dce7dc7588eb5b4307c1
Peter Dalby <peterdalby93@gmail.com>
2017-05-17 10:49:56 +0200

    Added navigation to all our pages.

--- js/common.js
+++ js/common.js
@@ -16,3 +16,47 @@ function getUrlParameter(sParam) {
         }
     }
 };
+
+
+function fnAddNavigation(){
+
+//Prepend the header navigation (1st item in body)
+
+$("body").prepend('<nav class="navigation">\
+\
+                  <a href="index.html" class="logolinkcontainer">\
+                    <img src="./img/techmarkLogoNoText.svg" alt="Techmark logo" class="logo"></img>\
+                  </a>\
+\
+                    <div class="navitems">\
+                        <ul>\
+                            <a href="index.html">Home</a>\
+                            <a href="searchResults.html">Events</a>\
+                            <a href="about-us.html">About us</a>\
+                            <a href="partners.html">Partners</a>\
+                            <a href="contact.html">Contact us</a>\
+                            <input type="search" class="navigationSearch" placeholder="Search for events...">\
+                        </ul>\
+                    </div>\
+                 </nav>');
+
+//Append the footer (last item in body)
+
+$("body").append('<footer class="pageFooter">\
+    <div class="leftfooter">\
+        <a href="index.html">News</a><br>\
+        <a href="index.html">Videos</a><br>\
+        <a href="searchResults.html">Events</a>\
+    </div>\
+    <div class="centerfooter">\
+        <a href="about-us.html">About Us</a><br>\
+        <a href="contact.html">Contact Us</a><br>\
+        <a href="partners.html">Become a Partner</a>\
+    </div>\
+    <div class="rightfooter">\
+        <input type="text" class="txtSubscribe" placeholder="Subscribe to the Techmark newsletter">\
+        <button class="btnSubscribe">Subscribe</button>\
+    </div>\
+</footer>')
+
+}
