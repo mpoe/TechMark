173af95e22f003e800f4b30709c9115e5a02cae8
Mpoe <Morten_p_oe@hotmail.com>
2017-05-17 10:31:41 +0200

    PRE FRANK

---
+++ js/common.js
@@ -0,0 +1,18 @@
+/* 
+    Function to get url parameters, from any value, e.g id, from, to etc.
+*/
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
